fc26de88707e038f6d176e965ea7c2e6adfacaf2
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2020-12-05 11:09:25 +0530

    fiished serach box component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends React.Component{
     constructor(){
         super();
         this.state = {
-            monsters : [
-            ]
+            monsters : [],
+            searchField:''
         };
     }
     
@@ -18,11 +19,21 @@ class App extends React.Component{
     }
 
     render(){
+
+        const{monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+            )
+
         return (
             <div className="App">
-            <CardList monsters={this.state.monsters} >
-            
-            </CardList>
+                
+                <SearchBox 
+                  placeholder='search monsters'
+                  handleChange={e => this.setState({searchField: e.target.value})}
+                />
+
+                <CardList monsters={filteredMonsters} />
               
             </div>
           );
