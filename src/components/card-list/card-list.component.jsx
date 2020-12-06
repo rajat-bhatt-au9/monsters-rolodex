7cd9e4e05f767e2b209f0c848ef12d9c43e89762
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2020-12-06 15:55:09 +0530

    committing final changes

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,8 @@ import { Card } from '../card/card.component';
 export const CardList = props =>(
     
     <div className='card-list'>
-        {props.monsters.map(monster => (<Card key={monster.id} monster={monster} />
+        {props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
         ))}
     </div>
 );
